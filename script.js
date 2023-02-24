bdd53d107498ffd1a84fb325a75265bd8bd928d1
Geraldo PMJ <geraldo.pmj@gmail.com>
2023-02-24 11:12:18 -0300

    refactored according to clean code

--- script.js
+++ script.js
@@ -1,60 +1,84 @@
-const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1b30c32fa88c3ac53722e8ba4733e1f9&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?sort_by=popularity&api_key=1b30c32fa88c3ac53722e8ba4733e1f9&query=';
+const API_KEY = "1b30c32fa88c3ac53722e8ba4733e1f9";
+const API_URL_POPULAR =
+  `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const API_URL_SEARCH =
+  `https://api.themoviedb.org/3/search/movie?sort_by=popularity&api_key=${API_KEY}&query=`;
+const IMG_URL = "https://image.tmdb.org/t/p/w1280";
+
 const form = document.getElementById("form");
-const search = document.getElementById("search");
+const searchInput = document.getElementById("search");
 const main = document.getElementById("main");
 
-getMovies(API_URL);
+init();
 
-async function getMovies(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-
-  showMovies(data.results);
+function init() {
+  displayMovies(API_URL_POPULAR);
+  setupForm();
 }
 
-function showMovies(movies) {
+async function displayMovies(url) {
+  try {
+    const movies = await getMovies(url);
+    const movieElements = movies.map(createMovieElement);
+    renderMovies(movieElements);
+  } catch (error) {
+    console.error(error);
+    renderErrorMessage("Failed to load movies.");
+  }
+}
+
+async function getMovies(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.results;
+}
+
+function createMovieElement(movie) {
+  const { title, poster_path, vote_average, overview } = movie;
+  const movieElement = document.createElement("div");
+  movieElement.className = "movie";
+  movieElement.innerHTML = `
+    <img src="${IMG_URL + poster_path}" alt="${title}">
+    <div class="movieInfo">
+      <h3>${title}</h3>
+      <span class="${getClassByRating(vote_average)}">${vote_average}</span>
+    </div>
+    <div class="overview">
+      <h4>Overview</h4>
+      <p>${overview}</p>
+    </div>`;
+  return movieElement;
+}
+
+function renderMovies(movieElements) {
   main.innerHTML = "";
-  movies.forEach((movie) => {
-    const { title, poster_path, vote_average, overview } = movie;
-    const movieEL = document.createElement("div");
-    movieEL.classList.add("movie");
-    movieEL.innerHTML = `
-        <img src= "${IMG_PATH + poster_path}" alt="${title}">
-        <div class="movieInfo">
-            <h3>${title}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
-        <div class="overview">
-            <h4>Overview</h4>
-            <p>${overview}</p>
-        </div>`;
-    main.appendChild(movieEL);
+  movieElements.forEach((movieElement) => {
+    main.appendChild(movieElement);
   });
 }
 
-function getClassByRate(vote) {
-  if (vote > 8) {
+function renderErrorMessage(message) {
+  main.innerHTML = `<p>${message}</p>`;
+}
+
+function getClassByRating(rating) {
+  if (rating >= 8) {
     return "green";
-  } else if ((vote) => 5) {
+  } else if (rating >= 5) {
     return "yellow";
   } else {
     return "red";
   }
 }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const searchTerm = search.value;
-
-  if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + searchTerm);
-    search.value = "";
-  } else {
-    window.location.reload();
-  }
-});
+function setupForm() {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm) {
+      const url = API_URL_SEARCH + encodeURIComponent(searchTerm);
+      displayMovies(url);
+      searchInput.value = "";
+    }
+  });
+}
